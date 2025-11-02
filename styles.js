15930762d2c43b72cfbed6f7ca85318c6f253183
Stefan765 <76748796+Stefan765@users.noreply.github.com>
2025-11-02 20:50:26 +0100

    Update styles.js

--- styles.js
+++ styles.js
@@ -554,7 +554,7 @@ export const styles = `
     color: #ff4081;
     font-size: 1.3em;
     cursor: pointer;
-    z-index: 10;
+    z-index: 1;
     pointer-events: auto;
     transition: transform 0.2s, color 0.3s;
   }
